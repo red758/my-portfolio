e1def15ab91d88f2164f84e4962e3721fbae7cf0
red758 <red095821@gmail.com>
2026-07-16 17:24:36 +0530

    Increase radius for radial gradient effect

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ box.addEventListener("mousemove", (e) => {
     const rect = box.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-    const radius = 80;
+    const radius = 100;
 
     overlay.style.webkitMaskImage = `radial-gradient(circle ${radius}px at ${x}px ${y}px, white 0%, white 100%, transparent 100%)`;
     overlay.style.maskImage = `radial-gradient(circle ${radius}px at ${x}px ${y}px, white 0%, white 100%, transparent 100%)`;
