4e437bdc4f6fecc111a463e4fdf7e651effc5a7f
red758 <red095821@gmail.com>
2026-07-16 08:32:12 +0530

    Add mousemove effect to image overlay

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const box = document.querySelector(".img-box");
+const overlay = document.querySelector(".overlay");
+
+box.addEventListener("mousemove", (e) => {
+    const rect = box.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const radius = 80;
+
+    overlay.style.webkitMaskImage = `radial-gradient(circle ${radius}px at ${x}px ${y}px, white 0%, white 100%, transparent 100%)`;
+    overlay.style.maskImage = `radial-gradient(circle ${radius}px at ${x}px ${y}px, white 0%, white 100%, transparent 100%)`;
+});
